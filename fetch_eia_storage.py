4004233580666224c9f5bb5fbc3c847e93d04b7a
Hst332 <hst332@gmail.com>
2025-11-25 13:12:47 +0100

    Update fetch_eia_storage.py

--- fetch_eia_storage.py
+++ fetch_eia_storage.py
@@ -11,13 +11,10 @@ def fetch_storage():
     lines = r.text.splitlines()
     reader = csv.DictReader(lines)
 
-    # Erste Zeile = aktuellster Storage-Report
     latest = next(reader)
 
-    # TOTAL WORKING GAS (in Bcf)
     total_gas = latest["Total Working Gas"]
 
-    # Datum sauber formatieren
     date = latest["Week Ending"]
     date = datetime.strptime(date, "%m/%d/%Y").strftime("%Y-%m-%d")
 
@@ -27,7 +24,7 @@ def fetch_storage():
 if __name__ == "__main__":
     try:
         value, date = fetch_storage()
-        print(value)  # Nur den Wert, damit GH-Actions ihn sauber übernehmen können
+        print(value)
     except Exception as e:
         print("ERROR")
         raise e
