070ebee260d92dd1198bc37c4b1c94eb71974709
Hst332 <hst332@gmail.com>
2025-11-25 13:18:22 +0100

    Update fetch_eia_storage.py

--- fetch_eia_storage.py
+++ fetch_eia_storage.py
@@ -1,30 +1,17 @@
 import csv
 import requests
-from datetime import datetime
 
 CSV_URL = "https://ir.eia.gov/ngs/wngsr.csv"
 
-def fetch_storage():
+def debug_headers():
     r = requests.get(CSV_URL)
     r.raise_for_status()
 
     lines = r.text.splitlines()
-    reader = csv.DictReader(lines)
+    reader = csv.reader(lines)
 
-    latest = next(reader)
-
-    total_gas = latest["Total Working Gas"]
-
-    date = latest["Week Ending"]
-    date = datetime.strptime(date, "%m/%d/%Y").strftime("%Y-%m-%d")
-
-    return float(total_gas), date
-
+    headers = next(reader)
+    print("HEADERS:", headers)
 
 if __name__ == "__main__":
-    try:
-        value, date = fetch_storage()
-        print(value)
-    except Exception as e:
-        print("ERROR")
-        raise e
+    debug_headers()
