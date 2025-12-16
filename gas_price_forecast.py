a149c99552c0b338e0a82a38aa6abcc593aa0d66
Hst332 <hst332@gmail.com>
2025-12-16 12:56:11 +0100

    Update gas_price_forecast.py

--- gas_price_forecast.py
+++ gas_price_forecast.py
@@ -242,10 +242,10 @@ def build_features(df_prices: pd.DataFrame) -> Tuple[pd.DataFrame, Dict]:
         storage_df = storage_df[["Date", "Storage_Surprise_Z"]].rename(
             columns={"Date": "merge_Date"}
         )
-     df.index.name = "merge_Date"
-       left = df.reset_index()
+        df.index.name = "merge_Date"
+        left = df.reset_index()
        
-       df = (
+        df = (
            left.merge(storage_df, on="merge_Date", how="left")
            .set_index("merge_Date")
        )
